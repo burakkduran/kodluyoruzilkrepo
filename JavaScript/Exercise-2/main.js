366bbb4b1fc8d6d870da6c19e292a4eb3707b623
Burak Duran <burak.duraninfo@gmail.com>
2022-07-12 18:15:48 +0300

    JavaScript exercise-2 done

---
+++ JavaScript/Exercise-2/main.js
@@ -0,0 +1,18 @@
+// Var ile değişken tanımlama
+/* console.clear()
+
+var fullName = "Burak"
+console.log(fullName) */
+
+// Let ile değişken tanımlama
+
+let fullName = "Burak"
+console.log(fullName)
+
+fullName ="Furkan"
+console.log(fullName)
+
+fullName += " Duran"
+console.log(fullName)
+
+console.log(fullName+ " Duran")
