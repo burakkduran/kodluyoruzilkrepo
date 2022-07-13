ff5740a81d4cca75044e08559f3dd8281ca96517
Burak Duran <burak.duraninfo@gmail.com>
2022-07-13 23:47:51 +0300

    Continue the exercises

---
+++ JavaScript/Exercise-2/boolean.js
@@ -0,0 +1,19 @@
+let isActive = false;
+isActive= true;
+console.log(Boolean(isActive))
+
+/*
+
+"" = false
+0 = false
+-0 = false
+null = false
+
+
+
+"a" = true
+" " = true
+"123" = true
+
+
+*/
---
+++ JavaScript/Exercise-2/numbers.js
@@ -0,0 +1,13 @@
+let price = 100;
+console.log(price)
+let tax = 0.10
+console.log("Faiz: ",tax)
+let taxedPrice = tax*price + price
+console.log("Faizli fiyat= ", taxedPrice)
+
+let fiyat = 100
+console.log("Fiyat: ",fiyat)
+fiyat +=1 //Arttırma işlemi
+fiyat -=1 //Azaltma işlemi
+
+console.log("Fiyat: ",fiyat)
